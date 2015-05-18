be31bcbf97a36881c9a6165b1db2de176f3efa59
Iskandar Setiadi <iskandarsetiadi@students.itb.ac.id>
2015-05-18 10:19:24 +0700

    Update to testing code

--- mongodb/mongodb_testcase2.py
+++ mongodb/mongodb_testcase2.py
@@ -15,9 +15,9 @@ def main(args):
     client = MongoClient('52.74.132.58', 27017) # Nearest Server location
     db = client['tests_database']
     tests = db['tests_collection']
-    max_iteration = 10
+    max_iteration = 2000
     key_size = 10
-    value_size = 10
+    value_size = 100 * 1024
 
     print '** Starting benchmarking **'
     print '** Length key + value: %d byte(s)**' % (key_size + value_size)
@@ -39,12 +39,13 @@ def main(args):
     tx = 0 # time counter
     counter = 1
     for item in item_id:
+        value = "a" * value_size
         t0 = time.time()
-        tests.update_one({"_id": item}, {'$set': {'mongodbkey' : ('upd' + str(counter))}})
+        tests.update_one({"_id": item}, {'$set': {'mongodbkey' : value}})
         tx = tx + (time.time() - t0)
         counter = counter + 1
-    print 'Number of iteration: %d' % (max_iteration)
-    print 'Average elapsed time: %.10f us' % (tx / max_iteration * 1000000 - empty_timer)
+    print 'Number of iteration: %d' % (counter)
+    print 'Average elapsed time: %.10f us' % (tx / counter * 1000000 - empty_timer)
     
     print '--READ--'
     tx = 0 # time counter
@@ -54,8 +55,8 @@ def main(args):
         res = tests.find_one({"_id": item})
         tx = tx + (time.time() - t0)
         counter = counter + 1
-    print 'Number of iteration: %d' % (max_iteration)
-    print 'Average elapsed time: %.10f us' % (tx / max_iteration * 1000000 - empty_timer)
+    print 'Number of iteration: %d' % (counter)
+    print 'Average elapsed time: %.10f us' % (tx / counter * 1000000 - empty_timer)
     
     print '--DELETE--'
     tx = 0 # time counter
@@ -65,8 +66,8 @@ def main(args):
         tests.delete_one({"_id": item})
         tx = tx + (time.time() - t0)
         counter = counter + 1
-    print 'Number of iteration: %d' % (max_iteration)
-    print 'Average elapsed time: %.10f us' % (tx / max_iteration * 1000000 - empty_timer)
+    print 'Number of iteration: %d' % (counter)
+    print 'Average elapsed time: %.10f us' % (tx / counter * 1000000 - empty_timer)
     client.close()
 
 if __name__ == '__main__':
